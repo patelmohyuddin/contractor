61388a27edbdd0f7123563fe7d92a346cfae3c92
patelmohyuddin <36945291+patelmohyuddin@users.noreply.github.com>
2020-08-09 07:31:26 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: "AIzaSyCILJRzXHfDL5amBnmFkZ2ykoyp_LrYH8o",
-  authDomain: "contractor-68bdd.firebaseapp.com",
-  databaseURL: "https://contractor-68bdd.firebaseio.com",
-  projectId: "contractor-68bdd",
-  storageBucket: "contractor-68bdd.appspot.com",
-  messagingSenderId: "221242846742",
-  appId: "1:221242846742:web:45200833e2620c2297cf37",
-  measurementId: "G-66XH1JGX8X"
+   apiKey: AIzaSyB6r_JlESav4qPlH8Be2jNYFE7JSE7njbE",
+   authDomain: "contractor-68bd.firebaseapp.com",
+   databaseURL: "https://contractor-68bd.firebaseio.com",
+   projectId: "contractor-68bd",
+   storageBucket: "contractor-68bd.appspot.com",
+   messagingSenderId: "342793850500",
+   appId: "1:342793850500:web:8a8cbd49cd11960265fcd1"
+
 };
